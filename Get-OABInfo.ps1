baa73ba12b3c83c76aad1e4067fa2da17c3e3fba
Tim Kightly <tim@tkightly.uk>
2018-07-05 21:11:52 +0100

    Fixed $OABGuid not being fed to function running remotely

--- Get-OABInfo.ps1
+++ Get-OABInfo.ps1
@@ -1,7 +1,7 @@
 Param (
-    [array]$ComputerName = $env:computername,
+    [array]$ComputerName = $env:computername
     [parameter(Mandatory=$true)]
-    [string]$OABGuid
+    [string]$using:OABGuid
 )
 function Get-OABInfo () {
     New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS | Out-Null
@@ -9,11 +9,11 @@ function Get-OABInfo () {
     $OAB = New-Object -TypeName psobject
 
     $UserHives | ForEach-Object {
-        if (Test-Path "HKU:\$_\Software\Microsoft\Exchange\Exchange Provider\OABs\$OABGuid") {
+        if (Test-Path "HKU:\$_\Software\Microsoft\Exchange\Exchange Provider\OABs\$using:OABGuid") {
             $OAB = New-Object -TypeName psobject
-            $data = Get-ItemProperty "HKU:\$_\Software\Microsoft\Exchange\Exchange Provider\OABs\$OABGuid" | Select-Object -ExpandProperty 'OAB Last Modified Time'
+            $data = Get-ItemProperty "HKU:\$_\Software\Microsoft\Exchange\Exchange Provider\OABs\$using:OABGuid" | Select-Object -ExpandProperty 'OAB Last Modified Time'
             $OAB | Add-Member -MemberType NoteProperty -Name LastModified -Value (Get-Date -Format "dd/MM/yyyy HH:mm:ss" -Date ([DateTime]::FromFileTime( (((((($data[7] * 256 + $data[6]) * 256 + $data[5]) * 256 + $data[4]) * 256 + $data[3]) * 256 + $data[2]) * 256 + $data[1]) * 256 + $data[0]) ))
-            $OAB | Add-Member -MemberType NoteProperty -Name Name -Value (Get-ItemProperty "HKU:\$_\Software\Microsoft\Exchange\Exchange Provider\OABs\$OABGuid" | Select-Object -ExpandProperty 'OAB NameU')
+            $OAB | Add-Member -MemberType NoteProperty -Name Name -Value (Get-ItemProperty "HKU:\$_\Software\Microsoft\Exchange\Exchange Provider\OABs\$using:OABGuid" | Select-Object -ExpandProperty 'OAB NameU')
             $OAB | Add-Member -MemberType NoteProperty -Name UserName -Value (Get-ItemProperty "HKU:\$_\Volatile Environment" | Select-Object -ExpandProperty 'USERNAME')
             $OAB
         }
